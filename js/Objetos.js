fa05c53bfb90ebf62ecdb183806565e848acbefd
veritsss <ignacio.veraf@alumnos.ucentral.cl>
2026-09-20 14:21:01 -0300

    docs(javascript): Agregar y/o eliminar propiedades de un objeto

--- js/Objetos.js
+++ js/Objetos.js
@@ -1,7 +1,4 @@
 // Objetos
-const nombreProducto = "Monitor 20 pulgadas";
-const precio = 300;
-const disponible = true;
 
 //Aqui trabajamos a producto como un objeto, el cual en este caso incluye diferentes variables
 const producto = {
@@ -10,6 +7,14 @@ const producto = {
   disponible: true,
 };
 
+//console.log(producto);
+//console.log(producto.precio); // accedemos a una propiedad de un objeto, en este caso nos mostrará en la consola el valor de precio
+//console.log(producto["precio"]); // es lo mismo que la anterior pero menos usada
+
+// Agregar nuevas propiedades
+producto.imagen = "imagen.jpg"; // Se añade una nueva caracteristica al objeto 'producto'
+
+// Eliminar propiedades
+delete producto.disponible;
+
 console.log(producto);
-console.log(producto.precio); // accedemos a una propiedad de un objeto, en este caso nos mostrará en la consola el valor de precio
-console.log(producto["precio"]); // es lo mismo que la anterior pero menos usada
