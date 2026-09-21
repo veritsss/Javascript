30a1712f06a1348d8b8f3061c4f93664165e7651
veritsss <ignacio.veraf@alumnos.ucentral.cl>
2026-09-21 15:38:57 -0300

    docs(javascript): Agregar ejemplos de bucle for iterando numeros y arreglos de objetos

---
+++ js/24_For_Loop.js
@@ -0,0 +1,28 @@
+// For Loop
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(`el numero ${i} es par`);
+  } else {
+    console.log(`el numero ${i} es impar`);
+  }
+}
+
+const carrito = [
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Televisión 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
+];
+
+for (let i = 0; i < carrito.length; i++) {
+  console.log(carrito[i].nombre);
+}
+
+// While Loop
+
+// Do while Loop
